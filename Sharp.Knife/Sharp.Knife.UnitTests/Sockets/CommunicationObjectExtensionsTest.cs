f9bb2041435e8badb68c743d1c80de2f0b2a78ad
BByrd <bryanjebyrd@gmail.com>
2015-10-19 01:22:19 -0500

    Created extension class for ICommunicationObject. Created Unit Test for the extension class. This class will properly close the connection for any implementer of ICommunicationObject (abort vs close).

---
+++ Sharp.Knife/Sharp.Knife.UnitTests/Sockets/CommunicationObjectExtensionsTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ServiceModel;
+using Sharp.Knife.Sockets;
+using Moq;
+using Xunit;
+
+namespace Sharp.Knife.UnitTests.Sockets
+{
+    public class CommunicationObjectExtensionsTest
+    {
+        private readonly Mock<IFakeSocket> sut; // system under test
+
+        public CommunicationObjectExtensionsTest()
+        {
+            sut = new Mock<IFakeSocket>();
+            sut.Setup(x => x.GetData()).Returns(1);
+        }
+
+        [Fact]
+        public void ReturnTest()
+        {
+            Assert.Equal(1, sut.Object.Execute(client => client.GetData()));
+            sut.Verify(mock => mock.Abort(), Times.Never);
+            sut.Verify(mock => mock.Close(), Times.Once);
+        }
+
+        [Fact]
+        public void CommunicationExceptionTest()
+        {
+            sut.SetupGet(c => c.State).Returns(CommunicationState.Faulted);
+            sut.Setup(x => x.GetData()).Throws(new CommunicationException());
+            Assert.Throws<CommunicationException>(() => sut.Object.Execute(client => client.GetData()));
+            sut.Verify(mock => mock.Abort(), Times.AtLeastOnce);
+        }
+
+        [Fact]
+        public void TimeoutExceptionTest()
+        {
+            sut.SetupGet(c => c.State).Returns(CommunicationState.Faulted);
+            sut.Setup(x => x.GetData()).Throws(new TimeoutException());
+            Assert.Throws<TimeoutException>(() => sut.Object.Execute(client => client.GetData()));
+            sut.Verify(mock => mock.Abort(), Times.AtLeastOnce);
+        }
+
+        [Fact]
+        public void ExceptionTest()
+        {
+            sut.SetupGet(c => c.State).Returns(CommunicationState.Faulted);
+            sut.Setup(x => x.GetData()).Throws(new Exception());
+            Assert.Throws<Exception>(() => sut.Object.Execute(client => client.GetData()));
+            sut.Verify(mock => mock.Abort(), Times.AtLeastOnce);
+        }
+
+        [Fact]
+        public void FaultedInFinallyTest()
+        {
+            sut.SetupGet(c => c.State).Returns(CommunicationState.Faulted);
+            sut.Object.Execute(client => client.GetData());
+            sut.Verify(mock => mock.Abort(), Times.Once);
+        }
+
+        [Fact]
+        public void FinallyTest()
+        {
+            sut.Object.Execute(client => client.GetData());
+            sut.Verify(mock => mock.Close(), Times.Once);
+        }
+    }
+
+    public interface IFakeSocket : ICommunicationObject
+    {
+        int GetData();
+    }
+}
---
+++ Sharp.Knife/Sharp.Knife/Sockets/CommunicationObjectExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Sharp.Knife.Sockets
+{
+    public static class CommunicationObjectExtensions
+    {
+        /// <summary>
+        /// Executes the method on the ICommunicationObject method. 
+        /// Will close or abort the client based on the exception and communication state.
+        /// </summary>
+        /// <typeparam name="TClient">ICommunicationObject type.</typeparam>
+        /// <typeparam name="TResponse">The return of the method.</typeparam>
+        /// <param name="client">ICommunicationObject implementer.</param>
+        /// <param name="method">The method from the ICommunicationObject object to execute.</param>
+        /// <returns>The value of TypeResponse from the method parameter.</returns>
+        /// <exception cref="CommunicationException">Error thrown if client is unable to communicate to endpoint.</exception>
+        /// <exception cref="TimeoutException">Error thrown if the alotted time to connect is exceeded.</exception>
+        public static TResponse Execute<TClient, TResponse>
+            (this TClient client, Func<TClient, TResponse> method)
+            where TClient : class, ICommunicationObject
+        {
+            if ((client == null) || (method == null))
+                return default(TResponse);
+
+            try
+            {
+                return method(client);
+            }
+            catch (CommunicationException)
+            {
+                client.Abort();
+                throw;
+            }
+            catch (TimeoutException)
+            {
+                client.Abort();
+                throw;
+            }
+            catch (Exception)
+            {
+                if (client.State == CommunicationState.Faulted)
+                    client.Abort();
+
+                throw;
+            }
+            finally
+            {
+                if (client.State != CommunicationState.Faulted)
+                {
+                    client.Close();
+                }
+                else
+                {
+                    client.Abort();
+                }
+            }
+        }
+    }
+}
